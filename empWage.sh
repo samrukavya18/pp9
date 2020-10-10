6a2388df3bf5342ba0d7311c712ec187d3e05bb4
samrukavya18 <samrukavya18@gmail.com>
2020-10-10 18:58:30 +0530

    Add Attendance

--- empWage.sh
+++ empWage.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
  
 echo "WELCOME"
+function attendance() {
+
+Emp=$((RANDOM%2))
+
+echo $Emp
+if [ $Emp == 1 ]
+then
+     Echo "Employee is Present"
+else
+     Echo "Employee is Absent"
+fi
+
+}
