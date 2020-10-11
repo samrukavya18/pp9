ad3878653e7a45d35b3b8449eef94ea3e230e303
samrukavya18 <samrukavya18@gmail.com>
2020-10-11 20:35:41 +0530

    Refactor Working hour function

--- empWage.sh
+++ empWage.sh
@@ -16,33 +16,33 @@ fi
 }
 attendance
 
-IS_PART_TIME=1
-IS_FULL_TIME=2
-MAX_HRS_IN_MONTH=100
-EMP_RATE_PER_HR=20
-NUM_WORKING_DAYS=20
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-totalEmpHr=0
-totalWorkingDays=0
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
-        $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
-do
-        ((totalWorkingDays++))
-        empCheck=$((RANDOM%3))
-        case $empCheck in
+totalWorkHours=0;
+totalWorkingDays=0;
+function getWorkingHours() {
+        case $1 in
                 $IS_FULL_TIME)
-                        empHrs=8
+                        workHours=8
                         ;;
                 $IS_PART_TIME)
-                        empHrs=4
+                        workHours=4
                         ;;
                 *)
-                        empHrs=0
+                        workHours=0
                         ;;
         esac
-
-totalEmpHr=$(($totalEmpHr+$empHrs))
+}
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        getWorkingHours $((RANDOM%3))
+        totalWorkHours=$(($totalWorkHours + $workHours))
 done
 
-totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HR));
+totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
 echo "Total Salary : $totalSalary"
